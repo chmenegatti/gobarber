335b4910fdadfef683abe02126621e693f4913b7
Cesar Menegatti <cesinha51@gmail.com>
2019-10-30 18:14:08 -0300

    Controllers Done. Error handler and other funcs

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -13,7 +17,7 @@ class AppointmentController {
         canceled_at: null,
       },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
@@ -45,6 +49,14 @@ class AppointmentController {
     }
 
     const { provider_id, date } = req.body;
+
+    /**
+     * Check if provider is scheduling himself
+     */
+
+    if (provider_id === req.userId) {
+      return res.status(401).json({ error: 'You cannot schedule yourself' });
+    }
 
     /**
      * Check if provider_id is a provider
@@ -95,6 +107,56 @@ class AppointmentController {
       provider_id,
       date: hourStart,
     });
+
+    /**
+     * Notification appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às 'H:mm'h'",
+      { locale: ptBR }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
+    });
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: 'You dont have permission to cancel this appointment' });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointment with 2 hours advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
+    await Queue.add(CancellationMail.key, { appointment });
     return res.json(appointment);
   }
 }
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://6975e38af30045a0bc4461c32b2b820b@sentry.io/1802725',
+};
